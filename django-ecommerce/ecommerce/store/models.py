56f01b03ba1f591c5ed88b85ed36f3c75caaaf58
wandersondevops <136174695+wandersondevops@users.noreply.github.com>
2024-07-27 06:21:47 -0400

    Products model create

--- django-ecommerce/ecommerce/store/models.py
+++ django-ecommerce/ecommerce/store/models.py
@@ -6,5 +6,27 @@ class Category(models.Model):
     name = models.CharField(max_length=255, db_index=True)
     slug = models.SlugField(max_length=255, unique=True)
 
+    class Meta:
+        verbose_name_plural = 'categories'
+
     def __str__(self):
         return self.name
+    
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    brand = models.CharField(max_length=255, default='un-branded')
+    description = models.TextField(blank=True)
+    slug = models.SlugField(max_length=255)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    #image = models.ImageField(upload_to='images/')
+
+    class Meta:
+        verbose_name_plural = 'products'
+
+
+
+
+
+    def __str__(self):
+        return self.title
