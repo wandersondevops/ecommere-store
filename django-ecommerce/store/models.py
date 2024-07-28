1179a9363a2f864ca5294285c07bcff035f8949e
wandersondevops <136174695+wandersondevops@users.noreply.github.com>
2024-07-28 04:42:03 -0400

    Updating models

--- django-ecommerce/store/models.py
+++ django-ecommerce/store/models.py
@@ -14,6 +14,7 @@ class Category(models.Model):
     
 
 class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=255)
     brand = models.CharField(max_length=255, default='un-branded')
     description = models.TextField(blank=True)
