caa34d956ca1cc78df7252a4bef29d46a775b7a3
wandersondevops <136174695+wandersondevops@users.noreply.github.com>
2024-07-28 04:59:26 -0400

    create render to the products

--- django-ecommerce/store/views.py
+++ django-ecommerce/store/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 
-from. models import Category
+from. models import Category, Product
 
 
 def store(request):
 
-    return render(request, 'store/store.html')
+    all_products = Product.objects.all() # Get all products from the database
+
+    context = {'all_products': all_products} # Create a dictionary with all products
+
+    return render(request, 'store/store.html', context=context) # Render the store.html template with the context dictionary
 
 
 def categories(request):
     all_categories = Category.objects.all()
 
     return {'all_categories': all_categories}
+
